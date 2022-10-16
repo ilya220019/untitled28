f96d7b9e7d013a349cfd69852a3718288f27eb26
ilya22019 <piripi349@gmail.com>
2022-10-16 21:24:12 +0600

    h

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<String> A = new ArrayList<>();
+        A.add("A1");
+        A.add("A22");
+        A.add("A333");
+        A.add("A4444");
+        A.add("A55555");
+        System.out.println(A);
+        ArrayList<String> B = new ArrayList<>();
+        B.add("B1");
+        B.add("B22");
+        B.add("B333");
+        B.add("B4444");
+        B.add("B55555");
+        ArrayList<String> C = new ArrayList<>();
+        Iterator<String> a = A.iterator();
+        Iterator<String> b = B.iterator();
+        Collections.reverse(B);
+        while (a.hasNext() && b.hasNext()){
+            C.add(a.next());
+            C.add(b.next());
+
+        }
+        System.out.println(C);
+        Collections.sort(C);
+        System.out.println(C);
     }
 }
